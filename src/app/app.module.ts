3358a70fd529b3bf465526b780b5b809504e119e
Sinner81120 <54507793+Sinner81120@users.noreply.github.com>
2019-10-05 16:10:22 +0700

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'home', component: HomeComponent },
+      { path: '**', redirectTo: 'products', pathMatch: 'full' },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
