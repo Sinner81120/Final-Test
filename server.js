3c18dffe2e9afcca8a39c87c3b8acb99effe64ab
Sinner81120 <54507793+Sinner81120@users.noreply.github.com>
2019-10-05 22:13:51 +0700

    fix

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/FinalTest/'));
+app.use(express.static(__dirname + '/dist/Final-test/'));
 
 app.get('/*' , function(req , res) {
-    res.sendFile(path.join(__dirname + '/dist/FinalTest/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Final-test/index.html'));
 });
 
 app.listen(process.env.PORT || 8080 );
