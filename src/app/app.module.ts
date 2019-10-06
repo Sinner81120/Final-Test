53403754f07d9fda3ff929362b3ea44117d6ab59
Sinner81120 <54507793+Sinner81120@users.noreply.github.com>
2019-10-06 13:15:31 +0700

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { DangnhapComponent } from './dangnhap/dangnhap.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ThiComponent } from './thi/thi.component';
 import { DangthiComponent } from './dangthi/dangthi.component';
+import { DoimatkhauComponent } from './doimatkhau/doimatkhau.component';
+import { QuenpassComponent } from './quenpass/quenpass.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { DangthiComponent } from './dangthi/dangthi.component';
     DangnhapComponent,
     AboutusComponent,
     ThiComponent,
-    DangthiComponent
+    DangthiComponent,
+    DoimatkhauComponent,
+    QuenpassComponent
   ],
   imports: [
     BrowserModule,
@@ -32,6 +36,8 @@ import { DangthiComponent } from './dangthi/dangthi.component';
       { path: 'About-us', component: AboutusComponent },
       { path: 'Testing/:subjectId', component: DangthiComponent },
       { path: 'Test', component: ThiComponent },
+      { path: 'ChangePassword', component: DoimatkhauComponent },
+      { path: 'ForgetPass', component: QuenpassComponent },
       { path: 'Register', component: DangkiComponent },
       { path: 'Login', component: DangnhapComponent },
       { path: '**', redirectTo: 'products', pathMatch: 'full' },
---
+++ src/app/quenpass/quenpass.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuenpassComponent } from './quenpass.component';
+
+describe('QuenpassComponent', () => {
+  let component: QuenpassComponent;
+  let fixture: ComponentFixture<QuenpassComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QuenpassComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuenpassComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
